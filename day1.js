37eca9589445519afd30e8ee0de98a80129fc5ab
bashisthanayak01 <bashisthanayak01@gmail.com>
2026-04-22 22:18:35 +0530

    start prc

---
+++ day1.js
@@ -0,0 +1,17 @@
+//Day 1 — Variables, Functions, Arrays
+let studentArry = [{ name: "r1", marks: 16 }, { name: "r2", marks: 46 }, { name: "r3", marks: 26 }, { name: "r4", marks: 56 }, { name: "r5", marks: 96 }];
+
+
+//print all name
+
+studentArry.map((names) => console.log(names.name));
+
+//who passed (marks > 40)
+let res = studentArry.filter((ele)=>ele.marks>40);
+
+console.log(res)
+
+//get total marks
+
+// let total = studentArry.reduce((ele,x)=>ele.marks+x);
+// console.log(total)
